2a80b54d57cbd726b903bf69d4faeb03415b1c27
ArinoMichi <cutesyprogrammer@gmail.com>
2024-01-09 14:08:24 +0100

    mucho avance

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  imageUrlUnete = 'https://images.milanuncios.com/api/v1/ma-ad-media-pro/images/05b4dd9b-d5bf-4e7d-acd7-6abdfb0a1159?rule=hw396_70';
-  imageUrlSolicita = 'https://images.milanuncios.com/api/v1/ma-ad-media-pro/images/1b0db5e9-1234-4af4-bc91-89d8ac74c2ba?rule=hw396_70';
+  imageUrlUnete = '../assets/images/highres_470954457-scaled.jpeg';
+  imageUrlSolicita = '../assets/images/computer_classroom_00.jpg';
+  imageUrlQuienesSomos = '../assets/images/cropped-image-8.png';
 }
