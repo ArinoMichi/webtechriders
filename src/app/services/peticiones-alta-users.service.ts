36b94caedd0667f41b75c32b54833b481b76699c
Rafael <librerorafael@outlook.com>
2024-01-19 12:42:02 +0100

    Arreglo peticion al registrarse

--- src/app/services/peticiones-alta-users.service.ts
+++ src/app/services/peticiones-alta-users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { PeticionAltaUsers } from '../models/peticion-alta-users.model';
@@ -11,9 +11,11 @@ export class PeticionesAltaUsersService {
   constructor(private _http: HttpClient) {}
 
   postPeticionAlta(id: number): Observable<any> {
-    var request = 'api/PeticionesAltaUsers?iduser='+id;
+    var request = 'api/PeticionesAltaUsers'
     var url = environment.urlApi + request;
+    var params = new HttpParams()
+    .append('iduser', id)
 
-    return this._http.get(url);
+    return this._http.post(url, {params: params});
   }
 }
