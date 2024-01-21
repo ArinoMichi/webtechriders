1f10a0db6d15651bde2a319a8dccb962d6036fc4
ArinoMichi <cutesyprogrammer@gmail.com>
2024-01-21 21:32:19 +0100

    more paneltr

--- src/app/components/paneltr/paneltr.component.ts
+++ src/app/components/paneltr/paneltr.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./paneltr.component.css']
 })
 export class PaneltrComponent {
+  datos = [
+    { dato1: 'Valor 1', dato2: 'Valor 2', dato3: 'Valor 3' },
+    // Agrega más filas según tus datos
+  ];
+  editarItem(index: number): void {
+    // Lógica para editar el ítem en el índice dado
+    console.log('Editar ítem en el índice', index);
+  }
 
+  eliminarItem(index: number): void {
+    // Lógica para eliminar el ítem en el índice dado
+    console.log('Eliminar ítem en el índice', index);
+    this.datos.splice(index, 1);
+  }
 }
