6e0e5e04ddd44afc5a1abe3512e4ce0c3e6de520
SejoMont <sejo.monterocastro@gmail.com>
2024-01-22 18:15:28 +0100

    charlas añadidas

--- src/app/components/calendario/calendario.component.ts
+++ src/app/components/calendario/calendario.component.ts
@@ -1,19 +1,56 @@
-import { Component } from '@angular/core';
+// calendario.component.ts
+import { Component, OnInit } from '@angular/core';
 import { CalendarEvent, CalendarMonthViewDay } from 'angular-calendar';
+
+import { CharlasService } from 'src/app/services/charlas.service';
+
+import { Charla } from 'src/app/models/charla.model';
+
+interface MyCalendarEvent extends CalendarEvent {
+  charla: Charla;
+}
 
 @Component({
   selector: 'app-calendario',
   templateUrl: './calendario.component.html',
   styleUrls: ['./calendario.component.css']
 })
-export class CalendarioComponent {
+export class CalendarioComponent implements OnInit {
+  public charlas!: Array<Charla>;
+
+  constructor(
+    private _charlasService: CharlasService
+  ) { }
+
   viewDate: Date = new Date();
-  events: CalendarEvent[] = [
-    // Tu lista de eventos aquí
-  ];
+  events: MyCalendarEvent[] = [];
+
+  ngOnInit(): void {
+    this._charlasService.getCharlas().subscribe((response) => {
+      this.charlas = response;
+      this.mapCharlasToEvents();
+    });
+  }
+
+  mapCharlasToEvents(): void {
+    this.events = this.charlas.map(charla => ({
+      title: charla.descripcion,
+      start: new Date(charla.fechaCharla),
+      end: new Date(charla.fechaCharla),
+      charla: charla,
+    }));
+  }
 
   dayClicked(day: CalendarMonthViewDay): void {
-    // Acciones al hacer clic en un día
     console.log('Día clickeado', day);
+  }
+
+  eventClicked(event: { event: CalendarEvent<any> }): void {
+    console.log('Evento clickeado', event);
+    // Intentaremos acceder a la propiedad charla dentro del método
+    if ('charla' in event.event) {
+      const charlaSeleccionada: Charla = (event.event as MyCalendarEvent).charla;
+      // Hacer algo con la Charla seleccionada...
+    }
   }
 }
