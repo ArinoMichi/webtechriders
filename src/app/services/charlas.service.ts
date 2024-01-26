590258fb57673f3c54246350f3aa7db968a3c97e
ArinoMichi <cutesyprogrammer@gmail.com>
2024-01-26 11:32:59 +0100

    fixed getCharlasTechRider

--- src/app/services/charlas.service.ts
+++ src/app/services/charlas.service.ts
@@ -109,10 +109,9 @@ export class CharlasService {
   }
 
   getCharlasTechRider(idTechRider: number): Observable<any> {
-    var request = 'api/querytools/charlastechrider';
+    var request = 'api/querytools/charlastechrider/' + idTechRider;
     var url = environment.urlApi + request;
-    var params = new HttpParams().append('idtechrider', idTechRider);
-    return this._http.get(url, { params: params });
+    return this._http.get(url);
   }
 
   getCharlasPendientesTecnologiasTechrider(token: string): Observable<any> {
