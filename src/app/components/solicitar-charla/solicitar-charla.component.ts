390f2ba6bea666c0b43d6deb0113fbc671c21317
Rafael <librerorafael@outlook.com>
2024-01-29 21:38:47 +0100

    solicitar charla

--- src/app/components/solicitar-charla/solicitar-charla.component.ts
+++ src/app/components/solicitar-charla/solicitar-charla.component.ts
@@ -2,6 +2,9 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ProvinciasService } from 'src/app/services/provincias.service';
 import { Provincia } from 'src/app/models/provincia.model';
 import { Charla } from 'src/app/models/charla.model';
+import { Curso } from 'src/app/models/curso.model';
+import { CursosService } from 'src/app/services/cursos.service';
+import { CharlasService } from 'src/app/services/charlas.service';
 
 @Component({
   selector: 'app-solicitar-charla',
@@ -18,17 +21,28 @@ export class SolicitarCharlaComponent implements OnInit{
   @ViewChild('cajaprovincia') cajaProvinciaRef!: ElementRef
 
   public provincias!: Array<Provincia>
+  public cursos!: Array<Curso>
   public charla!: Charla
+  public identity: any
+  public token: any
 
   constructor(
     private _ProvinciasService: ProvinciasService,
+    private _CursosService: CursosService,
+    private _CharlasService: CharlasService,
   )
-  {}
+  {
+    this.identity = JSON.parse(localStorage.getItem('identity') || '{}')
+    this.token = localStorage.getItem('token')
+  }
 
   ngOnInit(): void {
     this._ProvinciasService.getProvincias().subscribe((response) => {
       this.provincias = response;
     });
+    this._CursosService.getAllCursosFromProfesor(this.identity.idusuario).subscribe((response) => {
+      this.cursos = response;
+    })
   }
 
   enviarCharla(): void {
@@ -44,5 +58,8 @@ export class SolicitarCharlaComponent implements OnInit{
     this.charla = new Charla (0, descripcion, 2, fecha, observaciones, 0, 
       fechaSolicitud, turno, modalidad, "", curso, provincia)
 
+    this._CharlasService.insertCharla(this.charla, this.token).subscribe((response) => {
+      
+    })  
   }
 }
