4ad0710b92afc5f651836113cbfef4013bc48347
Rafael <librerorafael@outlook.com>
2024-01-31 23:02:26 +0100

    navbar se cambia bien

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, DoCheck{
   title = 'webtechriders';
 
   localStorageContent: string | null = null;
 
   ngOnInit() {
     // Verifica si hay contenido en el localStorage
+    this.localStorageContent = localStorage.getItem('identity');
+  }
+
+  ngDoCheck(): void {
     this.localStorageContent = localStorage.getItem('identity');
   }
 }
